e9ef017d63344b76a1d1a31f3549040051e02f18
DevAman-sudo <devaman.net@gmail.com>
2021-03-18 14:48:15 +0545

    ready header design

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,28 @@
 // DOM elements //
-const toggleButton = document.getElementById('toggle_button');
-const navbarLinks = document.getElementById('navbar_links');
+const mainNavbar = document.getElementById('main_navbar');
+const menu = document.getElementById('menu');
+const navLinksUl = document.getElementById('nav_links_ul');
 
-toggleButton.addEventListener('click' , () => {
-   navbarLinks.classList.toggle('active'); 
+window.addEventListener('scroll', () => {
+
+    if (window.pageYOffset > 0) {
+        mainNavbar.style.transition = "0.3s ease-in";
+        mainNavbar.style.height = '6.5rem';
+        mainNavbar.classList.add('bg_black');
+        mainNavbar.classList.add('txt_white');
+    } else {
+        mainNavbar.style.transition = "0.3s ease-in";
+        mainNavbar.style.height = "7.5rem";
+        mainNavbar.classList.remove('bg_black');
+        mainNavbar.classList.remove('txt_white');
+    }
+
+});
+
+menu.addEventListener('click', () => {
+    if (navLinksUl.style.display == "none") {
+        navLinksUl.style.display = "flex";
+    } else {
+        navLinksUl.style.display = "none";
+    }
 });
