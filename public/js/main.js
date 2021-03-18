7924e527f4a02a6309f92fac0830c73f4d5d9259
DevAman-sudo <devaman.net@gmail.com>
2021-03-18 19:20:29 +0545

    header section update

--- public/js/main.js
+++ public/js/main.js
@@ -1,28 +1,10 @@
-// DOM elements //
-const mainNavbar = document.getElementById('main_navbar');
-const menu = document.getElementById('menu');
-const navLinksUl = document.getElementById('nav_links_ul');
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
 
-window.addEventListener('scroll', () => {
-
-    if (window.pageYOffset > 0) {
-        mainNavbar.style.transition = "0.3s ease-in";
-        mainNavbar.style.height = '6.5rem';
-        mainNavbar.classList.add('bg_black');
-        mainNavbar.classList.add('txt_white');
-    } else {
-        mainNavbar.style.transition = "0.3s ease-in";
-        mainNavbar.style.height = "7.5rem";
-        mainNavbar.classList.remove('bg_black');
-        mainNavbar.classList.remove('txt_white');
-    }
-
-});
-
-menu.addEventListener('click', () => {
-    if (navLinksUl.style.display == "none") {
-        navLinksUl.style.display = "flex";
-    } else {
-        navLinksUl.style.display = "none";
-    }
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+  links.forEach(link => {
+    link.classList.toggle("fade");
+  });
 });
