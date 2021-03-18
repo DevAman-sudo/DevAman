d502338e924805a266a21aa1b0481fe8b67b3912
DevAman-sudo <devaman.net@gmail.com>
2021-03-18 21:52:25 +0545

    navbar complete

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,21 @@
+// DOM elements //
+const mainNav = document.getElementById('main_nav');
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
 
+// navbar animation //
+window.addEventListener('scroll', () => {
+    if (window.pageYOffset > 0) {
+        mainNav.style.transition = "0.3s ease-in";
+        mainNav.style.height = '6.5rem';
+    } else {
+        mainNav.style.height = "7.5rem";
+    }
+});
+
+
+// Hamburger Menu //
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("open");
   links.forEach(link => {
